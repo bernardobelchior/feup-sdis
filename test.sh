46a3b0629d67020097c4799e8e5550e5387914c4
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-03-20 13:42:16 +0000

    Script working for linux. Still need to test on MacOS.

--- test.sh
+++ test.sh
@@ -6,16 +6,42 @@ if [ "$#" -lt 2 ]; then
 	exit 1;
 fi
 
-curPath=$(realpath $1)
+os=$(uname)
+
+if [ "$os" = "Linux" ]; then ##Figure out how to know terminal name
+	#terminal=$(ps -o 'cmd=' -p $(ps -o 'ppid=' -p $$))
+	#terminal=$(echo $terminal -e sh -c)
+	terminal=$(echo urxvt -e bash -c)
+elif [ "$os" = "Darwin" ]; then
+	terminal=$(echo open -a Terminal)
+else
+	exit 1
+fi
+
+modulePath=$(realpath $1)
+originalPath=$(realpath .)
+
+cd $1
 
 #rmiregistry
-urxvt -e "rmiregistry -J-Djava.rmi.server.codebase=file://$curPath" &
+echo "Lanching rmiregistry...."
+eval $terminal "\"rmiregistry -J-Djava.rmi.server.codebase=file://$modulePath\" &"
+
+sleep 1 #To be sure that the rmiregistry is running
 
 #Servers 
-urxvt -e "java server.Server 1.0 1 1 224.0.0.0 4445 224.0.0.1 4446 224.0.0.2 4447" &
-urxvt -e "java server.Server 1.0 2 2 224.0.0.0 4445 224.0.0.1 4446 224.0.0.2 4447" &
+echo "Launching server 1..."
+eval $terminal "\"java server.Server 1.0 1 1 224.0.0.0 4445 224.0.0.1 4446 224.0.0.2 4447; read\" &"
+
+echo "Launching server 2..."
+eval $terminal "\"java server.Server 1.0 2 2 224.0.0.0 4445 224.0.0.1 4446 224.0.0.2 4447; read\" &"
+
+sleep 1 #To be sure that the servers are all running
 
 #Client
-urxvt -e "java client.TestApp 1 $2 $3 $4" &
+echo "Launching client..."
+eval $terminal "\"java client.TestApp 1 $2 $3 $4; read\" &"
 
 wait
+
+cd $originalPath
